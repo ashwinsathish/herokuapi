21f40f4d784a956ccbbfee9338764a53e1f8750d
Ashwin <94456641+ashwinsathish@users.noreply.github.com>
2023-03-18 12:52:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
-from Flask import jsonify
+from flask import jsonify
 from urllib import request
 
 app = FastAPI()
